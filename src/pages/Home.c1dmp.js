d1a9c193365f4210e719ee975037b6a98ab02b19
yusuen <eeyusuen@gmail.com>
2024-09-21 16:00:09 +0800

    first commit

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -1,8 +1,15 @@
 
 $w.onReady(function () {
+<<<<<<< HEAD
     const heading = $w("#headingElementID");
     heading.text = "Bring Your Gelato Dreams to Life";
     heading.font = "Poppins Black";
     heading.fontSize = 86;
     heading.color = "#000000";
+=======
+    // Write your JavaScript here
+    // To select an element by ID use: $w('#elementID')
+
+    // Click 'Preview' to run your code
+>>>>>>> 203ac55 (first commit)
 });
