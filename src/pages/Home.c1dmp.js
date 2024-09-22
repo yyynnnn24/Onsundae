0ae635566d974b6280c4c4a9b3cbc4a81c237565
yusuen <134473401+yyynnnn24@users.noreply.github.com>
2024-09-22 15:34:47 +0800

    Update Home.c1dmp.js

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -1,5 +1,3 @@
-// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
 $w.onReady(function () {
     // Write your JavaScript here
