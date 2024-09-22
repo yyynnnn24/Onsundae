0217f2fb1149aacee3bf060d58f5f95c8da0c73b
yusuen <eeyusuen@gmail.com>
2024-09-22 16:07:58 +0800

    add button

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -4,6 +4,11 @@ $w.onReady(function () {
     heading.text = "Bring Your Gelato Dreams to Life";
     heading.font = "Poppins";
     heading.fontSize = 86;
-    heading.color = "#000000"; // Hex for black
+    heading.color = "#404040";
+
+    const button = $w("#button8");
+    button.label = "Our Services";
+    button.onClick(() => {
+        wixLocation.to("/Services");
 });
 
