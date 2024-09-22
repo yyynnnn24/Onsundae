eea666822e61bb83d3e5c6a79f887545f19cd03f
yusuen <eeyusuen@gmail.com>
2024-09-22 16:16:03 +0800

    update button

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -6,9 +6,38 @@ $w.onReady(function () {
     heading.fontSize = 86;
     heading.color = "#404040";
 
+
     const button = $w("#button8");
     button.label = "Our Services";
+    button.color = "FFB83C";
+    button.font = "Wix Madefor Text";
+    button.fontSize = "25";
     button.onClick(() => {
         wixLocation.to("/Services");
+
+
+    const button = $w("#button5");
+    button.label = "Our Story";
+    button.color = "FFB83C";
+    button.font = "Poppins Extra Light";
+    button.fontSize = "20";
+    button.onClick(() => {
+        wixLocation.to("/About");
+
+
+    const button = $w("#button9");
+    button.label = "Find Us";
+    button.color = "FFB83C";
+    button.font = "Poppins Extra Light";
+    button.fontSize = "20";
+    button.onClick(() => {
+        wixLocation.to("/Contact Us");
+
+
+    const heading = $w("#text25");
+    heading.text = "We don’t just offer gelato, we create joyful experiences. Let us help you craft a unique dessert story!";
+    heading.font = "Poppins Extra Light";
+    heading.fontSize = 39;
+    heading.color = "#404040";
 });
 
