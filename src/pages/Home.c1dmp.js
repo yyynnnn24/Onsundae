f657449b2fcf556dfe90127e2433989c1d729d90
yusuen <eeyusuen@gmail.com>
2024-09-22 15:59:28 +0800

    update

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -1,7 +1,9 @@
-
 $w.onReady(function () {
-    // Write your JavaScript here
-    // To select an element by ID use: $w('#elementID')
-
-    // Click 'Preview' to run your code
+    // Add the heading element
+    const heading = $w("#text36");
+    heading.text = "Bring Your Gelato Dreams to Life";
+    heading.font = "Poppins";
+    heading.fontSize = 86;
+    heading.color = "#000000"; // Hex for black
 });
+
