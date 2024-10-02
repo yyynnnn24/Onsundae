e3a716c2067fe10eb40cf27d1fa2697f60b150be
yusuen <eeyusuen@gmail.com>
2024-10-02 19:30:45 +0800

    add button function

--- src/pages/New Page.usuui.js
+++ src/pages/New Page.usuui.js
@@ -2,9 +2,19 @@
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
 $w.onReady(function () {
-    // Write your JavaScript here
-
-    // To select an element by ID use: $w('#elementID')
-
-    // Click 'Preview' to run your code
+    $w.onReady(function () {
+        $w("#trackButton").onClick(() => {
+            let trackingNumber = $w("#trackingInput").value;
+    
+            // Change state based on the tracking number
+            if (trackingNumber === "12345") {
+                $w("#multiStateBox").changeState("State1"); // In transit
+            } else if (trackingNumber === "67890") {
+                $w("#multiStateBox").changeState("State2"); // Delivered
+            } else {
+                $w("#multiStateBox").changeState("State3"); // Preparing for shipping
+            }
+        });
+    });
+    
 });
