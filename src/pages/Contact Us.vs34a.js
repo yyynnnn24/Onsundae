1eb4b4837a5fde034ec3b4d9c602bce31b374d8e
Kyra <kyraafia.fci@gmail.com>
2024-10-02 15:59:06 +0800

    updated contact us page

--- src/pages/Contact Us.vs34a.js
+++ src/pages/Contact Us.vs34a.js
@@ -2,8 +2,33 @@
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
 $w.onReady(function () {
-    // Write your JavaScript here
+    
+    $w('#title').text = "Reach Out to Us!";
 
+    
+    $w('#formBox').backgroundColor = "#ADD8E5";
 
-    // Click 'Preview' to run your code
+    
+    $w('#submitButton').onClick(() => {
+        
+        const firstName = $w('#firstName').value;
+        const lastName = $w('#lastName').value;
+        const email = $w('#email').value;
+        const phone = $w('#phone').value;
+        const message = $w('#message').value;
+
+        
+        if (firstName && lastName && email && phone && message) {
+            
+            console.log("Form submitted:", { firstName, lastName, email, phone, message });
+            
+            $w('#formBox').collapse(); 
+            $w('#successMessage').text = "Thanks, we received your submission."
+            $w('#successMessage').show(); 
+        } else {
+            
+            console.log("Please fill out all fields.");
+            $w('#errorMessage').show(); 
+        }
+    });
 });
